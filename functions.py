48ab784916f07983fa3f9002dd4fd723f5ab7eb6
David Lichacz <dlichacz@gmail.com>
2018-08-11 06:34:46 -0400

    Added new functions

--- functions.py
+++ functions.py
@@ -3,3 +3,13 @@ def bizdays_neg(calendar, date1, date2):
 		return calendar.bizdays(date1, date2)
 	except ValueError:
 		return -calendar.bizdays(date2, date1)
+
+def as_text(value):
+    if value is None:
+        return ""
+    return str(value)
+
+def adjust_width(sheet):
+	for column_cells in sheet.columns:
+		length = max(len(as_text(cell.value)) for cell in column_cells)
+		sheet.column_dimensions[column_cells[0].column].width = length
