528a1cb5bdfaf1a337a15c6f1e2d83960cfb39e8
David Lichacz <dlichacz@gmail.com>
2018-08-12 08:35:28 -0400

    Created constants file

---
+++ constants.py
@@ -0,0 +1,5 @@
+holidays = ['2017-10-09', '2017-12-25', '2017-12-26', '2018-01-01', '2018-02-19', '2018-03-30', '2018-05-21', \
+'2018-07-02', '2018-08-06', '2018-09-03', '2018-10-08', '2018-12-25', '2018-12-26','2019-01-01']
+
+sheet_columns = ['Gift Date Added', 'Gift Batch Number', 'Constituent ID', 'Name', 'Tribute Card Type', \
+'Tribute Type', 'Description', 'Date Pulled', 'Date Sent', 'Notes']
